87e8cc36a273023b304a63afad79a2428f825360
mitermitek <188499468+mitermitek@users.noreply.github.com>
2025-06-25 20:49:00 +0200

    feat: initialize Vue project with Vite, Tailwind CSS, and Remixicon

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script setup></script>
+
+<template>
+  <main class="container max-w-6xl mx-auto px-2 py-4 space-y-6">
+    <h1 class="text-3xl font-black">I'm Kevin Mitermite</h1>
+
+    <div>
+      <p><span class="font-bold">Full Stack Web Developer</span> based in Toulouse, France.</p>
+      <p>Currently working for <span class="font-bold">5 Degrés</span> 🟠</p>
+    </div>
+
+    <div class="flex space-x-4">
+      <a
+        href="https://github.com/mitermitek"
+        target="_blank"
+        rel="noopener noreferrer"
+        class="hover:text-gray-600 hover:underline"
+      >
+        <i class="ri-github-fill"></i> GitHub
+      </a>
+      <a
+        href="https://www.linkedin.com/in/mitermitek/"
+        target="_blank"
+        rel="noopener noreferrer"
+        class="hover:text-gray-600 hover:underline"
+      >
+        <i class="ri-linkedin-box-fill"></i> LinkedIn
+      </a>
+    </div>
+
+    <div>
+      <p class="text-sm text-neutral-500 italic">Portfolio under construction. Blog coming soon!</p>
+    </div>
+  </main>
+</template>
+
+<style scoped></style>
