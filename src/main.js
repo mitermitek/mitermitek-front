01a6eebabecca110cb9a820b2b11b4ad8b6301ee
mitermitek <188499468+mitermitek@users.noreply.github.com>
2025-06-26 10:19:38 +0200

    feat: add Matomo analytics integration and create .env file for configuration

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
+// filepath: /Users/kmitermite/Developer/mitermitek-front/src/main.js
 import './assets/styles/main.css'
 
 import { createApp } from 'vue'
+import VueMatomo from 'vue-matomo'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App)
+  .use(VueMatomo, {
+    host: import.meta.env.VITE_MATOMO_URL,
+    siteId: import.meta.env.VITE_MATOMO_SITEID,
+  })
+  .mount('#app')
+
+window._paq.push(['trackPageView'])
